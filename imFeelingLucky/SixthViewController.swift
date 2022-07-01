cddd5701cbffdb19ae5f2f6a3240e5781fb089b1
Vinni Guo <guovinni@gmail.com>
2022-07-01 10:19:58 -0500

    iteration w the astrology part done

--- imFeelingLucky/SixthViewController.swift
+++ imFeelingLucky/SixthViewController.swift
@@ -50,10 +50,44 @@ class SixthViewController: UIViewController {
        }
         
     func getZodiac (month:Int, day:Int) -> String {
-      if(month==1 && day >= 20 || month==2 && day <= 18) {
-        return(":aquarius: Aquarius")
-      }
-      return("no.")
+        if(month==1 && day > 19 || month==2 && day < 20) {
+                return("♒️ Aquarius")
+              }
+              else if(month==2 && day > 19 || month==3 && day < 21)
+              {
+                return ("♓️ Pisces")
+              }
+              else if(month==3 && day > 20 || month==4 && day < 21){
+                return ("♈️ Aries")
+              }
+              else if (month==4 && day > 19 || month==5 && day < 21){
+                return ("♉️ Taurus")
+              }
+              else if(month==5 && day > 20 || month==6 && day < 22){
+                return ("♊️ Gemini")
+              }
+              else if(month==6 && day > 21 || month==7 && day < 23){
+                return ("♋️ Cancer")
+              }
+              else if(month==7 && day > 22 || month==8 && day < 23){
+                return ("♌️ Leo")
+              }
+              else if(month==8 && day > 22 || month==9 && day < 23){
+                return ("♍️ Virgo")
+              }
+              else if(month==9 && day < 24 || month==10 && day < 24){
+                return ("♎️ Libra")
+              }
+              else if(month==10 && day < 25 || month==11 && day < 22){
+                return ("♏️ Scorpio")
+              }
+              else if(month==11 && day < 23 || month==12 && day < 22){
+                return ("♐️ Sagittarius")
+              }
+              else if (month==12 && day < 23 || month==1 && day > 20){
+                return ("♑️ Capricorn")
+              }
+              return("no.")
   //    ♈️ Aries (Ram): March 21–April 19
   //    ♉️ Taurus (Bull): April 20–May 20
   //    ♊️ Gemini (Twins): May 21–June 21
